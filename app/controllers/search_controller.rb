b96e9a6faecb6594e256a06443397d872508ca2e
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-01-03 23:07:46 -0500

    finished routiing search function, added event results view page with links to event show pages

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,20 +2,50 @@ require 'pry'
 require 'rest-client'
 require 'json'
 
+
 class SearchController < ApplicationController
-
-  def search
-    byebug
-    @keyword = params[:keyword]
-    @events = JSON.parse(RestClient.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=wCElOJlP8V5gpb6GGKmL3c9hKAva1dRq&size=20&keyword=#{@keyword.split.join('+')}"))
-    @venues = JSON.parse(RestClient.get("https://app.ticketmaster.com/discovery/v2/venues.json?apikey=wCElOJlP8V5gpb6GGKmL3c9hKAva1dRq&size=20&keyword=#{@keyword.split.join('+')}"))
-    @attractions = JSON.parse(RestClient.get("https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=wCElOJlP8V5gpb6GGKmL3c9hKAva1dRq&size=20&keyword=#{@keyword.split.join('+')}"))
-
-    redirect_to search_path(@keyword)
-  end
+  #
+  # def search
+  #   @keyword = params[:format]
+  #   @event_results = event_search()
+  #   render 'results'
+  # end
 
   def results
-    @keyword = params[:keyword]
+    # @keyword = params[:format]
+    @event_results = event_search()
+    @search_event_results = creates_event_instances(@event_results).map
+
+  end
+
+  def show
+  end
+
+
+  private
+
+  def event_search
+    keyword_search = params[:format].split.join("+").to_s
+    keyword_search_path = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=wCElOJlP8V5gpb6GGKmL3c9hKAva1dRq&size=20&keyword=#{keyword_search}"
+    JSON.parse(RestClient.get(keyword_search_path))
+  end
+
+  def creates_event_instances(json_results)
+    json_results["_embedded"]["events"].map do |e|
+      ev = Event.create(name: e["name"], sale_date: e["sales"]["public"]["startDateTime"], start_date: e["dates"]["start"]["dateTime"], venue: ven = Venue.find_or_create_by(name: e["_embedded"]["venues"][0]["name"]))
+      ven.update(city: e["_embedded"]["venues"][0]["city"]["name"])
+      if e["_embedded"]["attractions"]
+        e["_embedded"]["attractions"].each do |attr|
+          ev.attractions << Attraction.find_or_create_by(name: attr["name"])
+        end
       end
+      ev
+    end
+  end
+
+
+
+  # @venues = JSON.parse(RestClient.get("https://app.ticketmaster.com/discovery/v2/venues.json?apikey=wCElOJlP8V5gpb6GGKmL3c9hKAva1dRq&size=20&keyword=#{@keyword.split.join('+')}"))
+  # @attractions = JSON.parse(RestClient.get("https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=wCElOJlP8V5gpb6GGKmL3c9hKAva1dRq&size=20&keyword=#{@keyword.split.join('+')}"))
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   root to: "welcome#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post 'search', to: 'search#search'
+  # post 'search', to: 'search#search'
   get 'search', to: 'search#results'
+  post 'search', to: 'search#results'
 
 end
