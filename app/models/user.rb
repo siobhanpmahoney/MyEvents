c45bea85c6a4a1337773e88e0ab5b34daaae6f8c
github username <github email address>
2018-01-04 19:13:11 -0500

    created relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,24 @@
 class User < ApplicationRecord
+  has_many :relationships, dependent: :destroy
+  has_many :friends, through: :relationships
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :birth_date, presence: true
-  validates :email, presence: true
+  def friends
+
+
+  end
+
+
+  def add_friend(friend)
+    [Relationship.create(user_id: self.id, friend_id: friend.id),
+    Relationship.create(friend_id: self.id, user_id: friend.id)]
+  end
+
+  # validates :username, presence: true, uniqueness: true
+  # validates :password, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :birth_date, presence: true
+  # validates :email, presence: true
 
 end
